c2139eef978db8391fb813820b3a9223ebc0ac75
JJJEEEz <juanestebanperezvelazquez@gmail.com>
2025-10-07 11:47:56 -0600

    fix(sidebar): correct text color for sidebar links in dark mode

--- resources/views/layouts/includes/admin/sidebar.blade.php
+++ resources/views/layouts/includes/admin/sidebar.blade.php
@@ -30,7 +30,7 @@ $links = [
                 {{$link['header']}}
             </div>
         @else
-        <a href="{{ $link['href'] }}" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group {{ $link['active'] ? 'bg-gray-100' : '' }}">
+        <a href="{{ $link['href'] }}" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-black hover:bg-gray-100 dark:hover:bg-gray-700 group {{ $link['active'] ? 'bg-gray-100' : '' }}">
             <span class= "w-6 h-6 inline-flex justify-center items-center text-gray-500"> <i class="{{ $link['icon'] }}"></i></span>
             <span class="ms-3">{{ $link['name'] }}</span>
         </a>
